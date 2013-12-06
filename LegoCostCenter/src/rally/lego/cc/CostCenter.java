3473d90b5338a406305725b2c67311356d990111
kkale <kkale@rallydev.com>
2013-12-06 15:15:25 +0100

    Only update cost centers when the new cost centers are different from the ones already setup in Rally

--- LegoCostCenter/src/rally/lego/cc/CostCenter.java
+++ LegoCostCenter/src/rally/lego/cc/CostCenter.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringTokenizer;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -59,46 +58,22 @@ public class CostCenter {
 		try {
 			cs.api = new RallyRestApi(new URI("https://rally1.rallydev.com"),
 					subAdminUsername, subAdminPassword);
-			Set<User> usersToChange = cs.filterCSVRecords(csvFile);
-			cs.updateCostCenters(usersToChange);
+			Set<User> refs2Update = cs.filterCSVRecords(csvFile);
+			cs.updateCostCenters(refs2Update);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.printf(
+					"Error: File %s could not be found at the given location",
+					csvFile);
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	/**
-	 * Update all the users that need a change in CostCenter
-	 * 
-	 * @param usersToChange
-	 * @throws IOException
-	 */
-	private void updateCostCenters(Set<User> usersToChange) throws IOException {
-		if (usersToChange == null || usersToChange.isEmpty()) {
 			System.out
-					.println("All users have correct costcenters. No need to change...");
-			return;
+					.printf("Error: Could not connect to https://rally1.rallydev.com. Please check your internet connection");
+		} catch (IOException e) {
+			System.out
+					.printf("Error: File %s could not be read. Please check your permissions",
+							csvFile);
 		}
-		System.out.println("Starting updating users in Rally...");
-		JsonObject updateUser = new JsonObject();
-		for (User user : usersToChange) {
-			updateUser.addProperty("CostCenter", user.getCostCenter());
-			UpdateRequest updateRequest = new UpdateRequest(user.getRef(),
-					updateUser);
-			UpdateResponse updateResponse = api.update(updateRequest);
-			updateUser = updateResponse.getObject();
-			System.out.println(String.format(
-					"Updated User %s New Cost Center = %s", user.getUserName(),
-					updateUser.get("CostCenter").getAsString()));
-		}
-		System.out.println("Done updating users in Rally...");
+
 	}
 
 	/**
@@ -112,32 +87,10 @@ public class CostCenter {
 	public Set<User> filterCSVRecords(String csvFile)
 			throws URISyntaxException, IOException {
 
-		Set<User> csvSet = new HashSet<CostCenter.User>();
+		String username;
+		String costCenter;
 
-		BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-		String record = null;
-		StringTokenizer recordTK = new StringTokenizer(",");
-		String username = null;
-		String costCenter = null;
-
-		// read cost centers from the CSV
-		System.out.println("Starting reading the data from CSV...");
-		while ((record = reader.readLine()) != null) {
-			recordTK = new StringTokenizer(record);
-			if (recordTK.hasMoreTokens()) {
-				username = recordTK.nextToken();
-			}
-			if (recordTK.hasMoreTokens()) {
-				costCenter = recordTK.nextToken();
-			}
-			if (username == null || costCenter == null) {
-				throw new IOException(String.format(
-						"invalid record found: %s %s", username, costCenter));
-			}
-
-			csvSet.add(new User(username, costCenter, null));
-		}
-		System.out.println("Done reading the data from CSV...");
+		Map<String, String> csvUser2CostCenter = getRecordsFromCSV(csvFile);
 
 		// read cost centers from Rally
 		System.out.println("Starting extracting data from Rally...");
@@ -145,30 +98,88 @@ public class CostCenter {
 		request.setFetch(new Fetch("UserName", "_ref", "CostCenter"));
 
 		QueryResponse resp = api.query(request);
-		User rallyUser = null;
-		Set<User> rallyUpdates = new HashSet<CostCenter.User>();
-		
+		Set<User> users2Update = new HashSet<User>();
+
 		if (resp.wasSuccessful()) {
 
 			String ref = "";
+			String csvCostCenter = "";
 
 			for (JsonElement result : resp.getResults()) {
 				JsonObject juser = result.getAsJsonObject();
 				username = juser.get("UserName").getAsString();
 				costCenter = juser.get("CostCenter").getAsString();
 				ref = juser.get("_ref").getAsString();
-				rallyUser = new User(username, costCenter, ref);
-				// if the csv is not the same as the current state, add it to the update list
-				if (!csvSet.contains(rallyUser)) {
-					rallyUpdates.add(rallyUser);
+				csvCostCenter = csvUser2CostCenter.get(username);
+				if (csvCostCenter != null && !csvCostCenter.equals(costCenter)) {
+					users2Update.add(new User(username, csvCostCenter, ref));
 				}
+
 			}
 		}
-		System.out.println("Starting extracting data from Rally...");
 
-		// what is left in the set at this point is the users that need a change
-		// in cost center
-		return rallyUpdates;
+		return users2Update;
+
+	}
+
+	@SuppressWarnings("resource")
+	private Map<String, String> getRecordsFromCSV(String csvFile)
+			throws FileNotFoundException, IOException {
+		Map<String, String> csvUser2CostCenterMap = new HashMap<String, String>();
+
+		BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+		String record = null;
+		String[] recordTK = new String[2];
+		String username = null;
+		String costCenter = null;
+
+		// read cost centers from the CSV
+		System.out.println("Starting reading the data from CSV...");
+		while ((record = reader.readLine()) != null) {
+			recordTK = record.split(",");
+			username = recordTK[0];
+			costCenter = recordTK[1];
+			if (username == null || costCenter == null) {
+				throw new IOException(String.format("invalid record found: %s",
+						record));
+			}
+
+			csvUser2CostCenterMap.put(username, costCenter);
+		}
+		System.out.println("Finished reading the data from CSV...");
+		return csvUser2CostCenterMap;
+	}
+
+	/**
+	 * Update all the users that need a change in CostCenter
+	 * 
+	 * @param users2Update
+	 * @throws IOException
+	 */
+	private void updateCostCenters(Set<User> users2Update)
+			throws IOException {
+		if (users2Update == null || users2Update.isEmpty()) {
+			System.out
+					.println("All users have correct costcenters. No need to change...");
+			return;
+		}
+		System.out.println("Update Records: " + users2Update);
+		System.out.println("Starting updating users in Rally...");
+		String costCenter = null;
+		for (User user : users2Update) {
+			JsonObject updateUser = new JsonObject();
+			costCenter = user.getCostCenter();
+			if (costCenter == null) {
+				costCenter = "None";
+			}
+			updateUser.addProperty("CostCenter", costCenter);
+			UpdateRequest updateRequest = new UpdateRequest(user.getRef(), updateUser);
+			UpdateResponse updateResponse = api.update(updateRequest);
+			updateUser = updateResponse.getObject();
+			 System.out.println(String.format("Updated User %s New Cost Center = %s",
+			 user.getUserName(), user.getCostCenter()));
+		}
+		System.out.println("Done updating users in Rally...");
 	}
 
 	/**
@@ -223,43 +234,9 @@ public class CostCenter {
 		}
 
 		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + getOuterType().hashCode();
-			result = prime * result
-					+ ((costCenter == null) ? 0 : costCenter.hashCode());
-			result = prime * result
-					+ ((userName == null) ? 0 : userName.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			User other = (User) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			if (costCenter == null) {
-				if (other.costCenter != null)
-					return false;
-			} else if (!costCenter.equals(other.costCenter))
-				return false;
-			if (userName == null) {
-				if (other.userName != null)
-					return false;
-			} else if (!userName.equals(other.userName))
-				return false;
-			return true;
-		}
-
-		private CostCenter getOuterType() {
-			return CostCenter.this;
+		public String toString() {
+			return "User [userName=" + userName + ", costCenter=" + costCenter
+					+ "ref = " + ref + "]";
 		}
 
 	}
